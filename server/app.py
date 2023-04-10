7aae3645110e7f1a7a60bddec1a2b8166ace3307
Guilherme Oliveira <guimdo1999@hotmail.com>
2023-04-10 14:41:30 -0300

    Teste avançado
    Testando como fazer funcionar

--- server/app.py
+++ server/app.py
@@ -1,24 +1,28 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit
 from flask_cors import CORS
 import zmq
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, cors_allowed_origins="*")
-CORS(app)
+CORS(app, resources={r"/*":{"origins":"*"}})
 context = zmq.Context()
 socket = context.socket(zmq.SUB)
 
 @socketio.on('connect')
 def handle_connect():
+    print("----Conectou")
     socket.connect('tcp://localhost:5555')
-    socket.setsockopt_string(zmq.SUBSCRIBE, b'')
+    socket.setsockopt_string(zmq.SUBSCRIBE, '')
 
 @socketio.on('message')
-def handle_message(data): 
-    print("mensagem: "+ data)
-    socket.send_string(data)
+def handle_message(message): 
+    """ message = socket.recv_string() """
+    print("----Mensagem recebida: ", message)
+    print("----zeromq: ", socket.recv_string())
+    emit('message', message)
+    
     
 if __name__ == '__main__':
     socketio.run(app)
--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
 import { useEffect, useState } from "react";
 import io from "socket.io-client";
-const socket = io("http://localhost:5000");
 
+const socket = io("http://localhost:5000");
 function App() {
   const [message, setMessage] = useState("");
+  const [messageR, setMessageR] = useState<any>([]);
 
   useEffect(() => {
     socket.on("connect", () => {
       console.log("Conectado ao servidor.");
     });
-    socket.on('message', (data) => {
-      console.log(data);
-    })
+  }, []);
+
+  useEffect(() => {
+    socket.on("message", (data) => {
+      console.log("server: "+ data);
+      setMessageR((messageR: any) => [...messageR, data]);
+      /* return () => {
+        socket.disconnect();
+      }; */
+    });
   }, []);
 
   const handleMessageChange = (e: any) => {
@@ -19,14 +27,35 @@ function App() {
   };
   const handleSendMessage = () => {
     socket.emit("message", message);
+    console.log(message);
+  };
+
+  socket.on("message", (data) => {
+    setMessageR((messageR: any) => [...messageR, data]);
+    console.log(messageR);
+  })
+  
+  const handleDisconnect = () => {
+    console.log("dc");
+    socket.disconnect();
   };
 
   return (
     <div>
       <div>
-        <input type="text" value={message} onChange={(e) => handleMessageChange(e)} />
+        <input
+          type="text"
+          value={message}
+          onChange={(e) => handleMessageChange(e)}
+        />
         <button onClick={() => handleSendMessage()}>Enviar</button>
       </div>
+      <button onClick={() => handleDisconnect()}>DC</button>
+      <ul>
+        {messageR.map((msg: String, index: any) => (
+          <li key={index}>{msg}</li>
+        ))}
+      </ul>
     </div>
   );
 }
